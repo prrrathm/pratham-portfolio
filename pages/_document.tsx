344f7a381d72516b1f69e7d8b5e29d35d6639492
Pratham Sharma <prrrathm@gmail.com>
2023-11-08 19:09:01 +0530

    fix '

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html lang="en" >
+		<Html lang="en">
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com"></link>
 				<link
 					rel="preconnect"
 					href="https://fonts.gstatic.com"
-					crossOrigin
+					// crossOrigin={true}
 				></link>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Lora&display=swap"
